5527b98a6c568a2a8dd7ca4dca7b8210dd369f47
Adam Hurm <adamhurm@gmail.com>
2020-12-04 19:59:11 -0500

    temp/WIP commit, day4puzzle2 incomplete

--- day4_solution.ex
+++ day4_solution.ex
@@ -1,9 +1,9 @@
 defmodule Day4Solution do
 	def check_passports do
-		{:ok, file} = File.read("day4_given-passport_list.txt")
+		{:ok, file} = File.read("day4_test.txt")#_given-passport_list.txt")
 		passports = String.split(file, "\n\n")
-		check_passport_fields(passports, 0, Enum.count(passports)-1)
-		#validate_passport_fields(passports, 0, Enum.count(passports)-1)
+		#check_passport_fields(passports, 0, Enum.count(passports)-1)
+		validate_passport_fields(passports, 0, Enum.count(passports)-1)
 	end
 
 	def check_passport_fields(passports, valid, n) when n < 1 do
@@ -25,100 +25,115 @@ defmodule Day4Solution do
 	def validate_passport_fields(passports, valid, n) when n < 1 do
 		passports_stripped = String.replace(Enum.at(passports, n), "\n", " ")
 		passport_full = String.split(passports_stripped, " ", trim: true)
-		IO.puts(["Puzzle 2: ",Integer.to_string(valid + validate_passport_field(passport_full, n))," valid passports found."])
+		#TODO: check that all fields are present as well. this will require modifying Puzzle 1 function
+		if validate_fields(passport_full, Enum.count(passport_full)-1, true) do
+			IO.puts(["Puzzle 2: ",Integer.to_string(valid+1)," valid passports found."])
+		else
+			IO.puts(["Puzzle 2: ",Integer.to_string(valid)," valid passports found."])
+		end
 	end
 
 	def validate_passport_fields(passports, valid, n) do
 		passports_stripped = String.replace(Enum.at(passports, n), "\n", " ")
 		passport_full = String.split(passports_stripped, " ", trim: true)
-		IO.puts(["passport:",passport_full," size:",Integer.to_string(Enum.count(passport_full))])
-		validate_passport_fields(passports, valid + validate_passport_field(passport_full, Enum.count(passport_full)-1), n-1)
+		#IO.puts(["passport:",passport_full," size:",Integer.to_string(Enum.count(passport_full))])
+		#TODO: check that all fields are present as well. this will require modifying Puzzle 1 function
+		if validate_fields(passport_full, Enum.count(passport_full)-1, true) do
+			validate_passport_fields(passports, valid+1, n-1)
+		else
+			validate_passport_fields(passports, valid, n-1)
+		end
 	end
 	
-	#TODO: Need to edit this function so that we iterate through full passports instead of just the first element	
-	#TODO: Also need to fix warnings becuase of unsafe variable assignments in 'case' block 
-	def validate_passport_field(fields, n) do
+	def validate_fields(fields, n, valid) when n < 1 do
 		passport_fields = String.split(Enum.at(fields, n), ":", trim: true)
 		fieldKey = Enum.at(passport_fields, 0)
 		fieldVal = Enum.at(passport_fields, 1)
-		byrBool = false
-		iyrBool = false
-		eyrBool = false
-		hgtBool = false
-		hclBool = false
-		eclBool = false
-		pidBool = false
+		valid && validate_field(fieldKey,fieldVal)
+	end
+
+	def validate_fields(fields, n, valid) do
+		passport_fields = String.split(Enum.at(fields, n), ":", trim: true)
+		fieldKey = Enum.at(passport_fields, 0)
+		fieldVal = Enum.at(passport_fields, 1)
+		validate_fields(fields, n-1, valid && validate_field(fieldKey,fieldVal))
+	end
+
+	def validate_field(fieldKey, fieldVal) do
 		IO.puts(["key:",fieldKey,",val:",fieldVal])
 		case fieldKey do
 			"byr" ->
 				if (String.length(fieldVal) == 4) && (String.to_integer(fieldVal) >= 1920) && (String.to_integer(fieldVal) <= 2002) do
-					IO.puts("byr valid")
-					byrBool = true
+					#IO.puts("byr valid")
+					true
 				else
-					IO.puts("byr invalid")
+					#IO.puts("byr invalid")
+					false
 				end
 			"iyr" ->
 				if (String.length(fieldVal) == 4) && (String.to_integer(fieldVal) >= 2010) && (String.to_integer(fieldVal) <= 2020) do
-					IO.puts("iyr valid")
-					iyrBool = true
+					#IO.puts("iyr valid")
+					true
 				else
-					IO.puts("iyr invalid")
+					#IO.puts("iyr invalid")
+					false
 				end
 			"eyr" ->
 				IO.puts(String.length(fieldVal))
 				if (String.length(fieldVal) == 4) && (String.to_integer(fieldVal) >= 2020) && (String.to_integer(fieldVal) <= 2030) do
-					eyrBool = true
-					IO.puts("eyr valid")
+					#IO.puts("eyr valid")
+					true
 				else
-					IO.puts("eyr invalid")
+					#IO.puts("eyr invalid")
+					false
 				end
 			"hgt" ->
 				if String.contains?(fieldVal, "cm") do
 					hgt = String.to_integer(Enum.at(String.split(fieldVal,"cm"),0))
-					if hgt >= 150 && hgt <= 193 do
+					if (hgt >= 150) && (hgt <= 193) do
 						IO.puts("hgt valid")
-						hgtBool = true
+						true
 					else
 						IO.puts("hgt invalid")
+						false
 					end
 				end
 				if String.contains?(fieldVal, "in") do
 					hgt = String.to_integer(Enum.at(String.split(fieldVal,"in"),0))
-					if hgt >= 59 && hgt <= 76 do
+					if (hgt >= 59) && (hgt <= 76) do
 						IO.puts("hgt valid")
-						hgtBool = true
+						true
 					else
 						IO.puts("hgt invalid")
+						false
 					end
 				end
 			"hcl" ->
 				if Regex.match?(~r{\A\#[\dabcdef]*\z}, fieldVal) do 
 					IO.puts("hcl valid")
-					hclBool = true
+					true
 				else
 					IO.puts("hcl invalid")
+					false
 				end
 			"ecl" ->
 				if "amb blu brn gry grn hzl oth" =~ fieldVal do
 					IO.puts("ecl valid")
-					eclBool = true
+					true
 				else
 					IO.puts("ecl invalid")
+					false
 				end
 			"pid" ->
 				if (String.length(fieldVal) == 9) && Regex.match?(~r{\A\d*\z}, fieldVal) do
 					IO.puts("pid valid")
-					pidBool = true
+					true
 				else
 					IO.puts("pid invalid")
+					false
 				end		
-			"cid" -> {:cont, nil}
+			"cid" -> true
 			_ -> IO.puts("reached unexpected case")
-		end
-		if byrBool && iyrBool && eyrBool && hgtBool && hclBool && eclBool && pidBool do
-			1
-		else
-			0
 		end
 	end
 end
