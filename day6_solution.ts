0d5f7d4b58583cbb8614f96937693f3beee6f3f4
Adam Hurm <adamhurm@gmail.com>
2020-12-24 01:19:48 -0500

    day 6 puzzle 2

--- day6_solution.ts
+++ day6_solution.ts
@@ -1,11 +1,12 @@
 import { readFileSync } from 'fs';
 
+//split forms into families
 const customsForms = readFileSync('day6_given-customs_forms.txt','utf8');
-
 var splitForms = customsForms.split('\n\n');
 
-function checkTotalUniqueChars(listOfForms) {
+function checkAnyone(listOfForms) {
     let sumOfUniques = 0;
+    
     for(let i=0; i < listOfForms.length; i++) {
 	let singleLine = listOfForms[i].replace(/\r?\n|\r/g, '');
 	let uniqueOnly = singleLine.split('').filter(function(item, i, ar){ return ar.indexOf(item) === i; }).join('');
@@ -15,43 +16,45 @@ function checkTotalUniqueChars(listOfForms) {
     return sumOfUniques;
 }
 
-function checkFamilyUniqueChars(listOfForms) {
+function checkEveryone(listOfForms) {
     let sumOfMatches = 0;
+    
     for(let i=0; i < listOfForms.length; i++) {
 	let familyMembers = listOfForms[i].split('\n');
-	let matches = "";
-	for(let j=0; j < familyMembers.length-1; j++) {
-	    let matchesHolder = compareStrings(familyMembers[j], familyMembers[j+1], matches);
-	    console.log(matchesHolder);
-	    if(!matches.includes(matchesHolder)) {
-		matches = matches + matchesHolder
-		sumOfMatches = sumOfMatches + matches.length;
+	
+	//clean the final UNIX newline
+	if(familyMembers[familyMembers.length-1] == "") {
+	    familyMembers.splice(-1,1);
+	}
+	//a-z matches
+	let familyMatches = Array(26).fill(true);
+	
+	
+	//for each family member
+	for(let j=0; j < familyMembers.length; j++) {
+	    let familyMatchesTemp = Array(26).fill(false);
+	    
+	    //for every character
+	    for(let k=0; k < familyMembers[j].length; k++) {
+		if(familyMatches[familyMembers[j].charAt(k).charCodeAt(0) - 'a'.charCodeAt(0)]) {
+		    familyMatchesTemp[familyMembers[j].charAt(k).charCodeAt(0) - 'a'.charCodeAt(0)] = true;
+		}
+	    }
+	    familyMatches = [...familyMatchesTemp];
+	}
+	
+	let totalMatches = 0;
+	for(let j=0; j < 26; j++) {
+	    if (familyMatches[j]) {
+		//console.log(String.fromCharCode(j + 97));
+		totalMatches++;
 	    }
 	}
-	console.log("\n\n");
+	
+	sumOfMatches += totalMatches;
     }
+    return sumOfMatches;
 }
-
-function compareStrings(s1, s2, currentMatches) {
-    let shortStr;
-    let longStr;
-
-    if(s1.length < s2.length) {
-	shortStr = s1;
-	longStr = s2;
-    } else {
-	shortStr = s2;
-	longStr = s1;
-    }
-
-    for(let i = 0; i < shortStr.length; i++) {
-	if(longStr.indexOf(shortStr[i]) !== -1) {
-	    currentMatches = currentMatches + shortStr[i];
-	}
-    }
-    console.log(s1 + "||" + s2 + " : " + currentMatches);
-    return currentMatches.split('').sort().join('');
-}
-
-console.log("Puzzle 1: " + checkTotalUniqueChars(splitForms) + " is the sum of all customs forms \'yes\' responses.");
-//console.log("Puzzle 2: " + checkFamilyUniqueChars(splitForms) + " is the sum of each family's customs forms \'yes\' responses.");
+    
+console.log("Puzzle 1: " + checkAnyone(splitForms) + " is the sum of all customs forms \'yes\' responses.");
+console.log("Puzzle 2: " + checkEveryone(splitForms) + " is the sum of each family's customs forms \'yes\' responses.");
